33311d7d4984a05d9405b2fc9b0a0ca0d226abd1
Swati pardhi <85123416+swatipardhi03@users.noreply.github.com>
2024-07-30 14:59:22 +0530

    Add files via upload

---
+++ datetimemodule.py
@@ -0,0 +1,8 @@
+import time # This is required to include time module.
+ticks = time.time()
+print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
+
+import calendar
+cal = calendar.month(2023, 4)
+print ("Here is the calendar:")
+print (cal)
