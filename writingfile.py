b85793e10949a8886b56035b1d0aae245fa16973
Swati pardhi <85123416+swatipardhi03@users.noreply.github.com>
2024-07-30 13:05:22 +0530

    Add files via upload

---
+++ writingfile.py
@@ -0,0 +1,7 @@
+employee_file = open("employee.txt", "a")
+employee_file.write("\n Name:Pratiksha \n Phone number:928406509 \n Email: pratiksha@gmail.com")
+employee_file.close()
+
+'''employee_file = open("index.html","w")
+employee_file.write("<p>This are the employee details</p>")
+employee_file.close()'''
